52b4089eeeeebc2f27e02094d7c07b9a2b712a5a
colinBranca <colin.branca@epfl.ch>
2017-10-02 09:00:56 +0200

    Use operation overload in class definition

--- finiteField.py
+++ finiteField.py
@@ -1,48 +1,64 @@
-class FiniteField:
+def FiniteField(p):
 
-    def __init__(self, p):
-        self.p = p
+    class Field:
 
-    #return x mod p
-    def f(self, x):
-        return x % self.p
+        def __init__(self, n):
+            self.n = n % p
+            self.field = Field
 
-    #add two numbers mod p
-    def add(self, a, b):
-        return self.f(a + b)
+        #add two numbers mod p
+        def __add__(self, other):
+            return Field(self.n + other.n)
 
-    #return opposite (mod p) of input number
-    def opposite(self, a):
-        return self.f(self.p - a)
+        #return opposite (mod p) of input number
+        def __neg__(self):
+            return Field(p - self.n)
 
-    #substract two numbers mod p
-    def substract(self, a, b):
-        return self.f(a + self.opposite(b))
+        #substract two numbers mod p
+        def __sub__(self, other):
+            return self + (-other)
 
-    #multiply two numbers mod p
-    def multiply(self, a, b):
-        return self.f(a*b)
+        #multiply two numbers mod p
+        def __mul__(self, other):
+            return Field(self.n * other.n)
 
-    #return inverse (mod p) of input number
-    def inverse(self, a):
-        return self.f(euclide(self.p, a))
+        #return inverse (mod p) of input number
+        def inverse(self):
+            return Field(euclide(p, self.n))
 
-    #divide two numbers (mod p)
-    def divide(self, a, b):
-        return self.multiply(a, self.inverse(b))
+        #divide two numbers (mod p)
+        def __div__(self, other):
+            return self * other.inverse()
 
-    #return a^b (mod p)
-    def power(self, a, b):
-        if b == 0:
-            return 1
-        elif b == 1:
-            return self.f(a)
-        elif b % 2 == 0:
-            halfPower = self.power(a, b/2)
-            return self.multiply(halfPower, halfPower)
-        else:
-            halfPower = self.power(a, (b-1)/2)
-            return self.multiply(self.multiply(a, halfPower), halfPower)
+        #return a^b (mod p)
+        def __pow__(self, other):
+            #if the power is an integer
+            if type(other) is int:
+                power = other
+            #if the power is a FiniteField
+            elif isinstance(other, Field):
+                power = other.n
+            #else we do nothing
+            else:
+                return Field(0)
+
+            if power == 0:
+                return 1
+            elif power == 1:
+                return self
+            elif power % 2 == 0:
+                halfPower = self ** Field(power/2)
+                return halfPower * halfPower
+            else:
+                halfPower = self ** Field((power-1)/2)
+                return self * halfPower * halfPower
+
+        #return the field as a string
+        def __str__(self) :
+            return '%d mod(%d)' % (self.n, self.p)
+
+    Field.p = p
+    return Field
 
 #Find inverse of a number (mod p) using extented Euclide algorithm
 def euclide(a, b):
