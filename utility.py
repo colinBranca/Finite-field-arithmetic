b2c0ef4a3d8f66af149c38dcae620748e6f6b3d3
colinBranca <colin.branca@epfl.ch>
2017-10-06 11:20:28 +0200

    Implement Miller Rabin to test primality

--- utility.py
+++ utility.py
@@ -23,18 +23,19 @@ def euclide(a, b):
     res = helper(x, r, s, 1)
     return res if res > 0 else res
 
-def isPrime(x):
-    if x == 2:
+
+
+#test if number is prime or not
+def isPrime(n):
+    #test if n is a known prime
+    if n in (2, 3, 5, 7):
         return True
-    elif x % 2 == 0 or x == 1:
+    #test if n = 2^k
+    elif n % 2 == 0 or n == 1:
         return False
 
-    limit = sqrt(x)
-
-    for i in range(3, limit+1, 2):
-        if x % i == 0:
-            return False
-    return True
+    #use miller rabin method to test primality
+    return millerRabin(n)
 
 
 #return the square root of a number rounded to lower value
@@ -47,3 +48,42 @@ def sqrt(a):
         dif = abs(x1 - x2)
         x1 = x2
     return int(x1)
+
+#miller rabin primality test
+def millerRabin(n):
+    #for n odd, at least 3/4 of intergers a, 1 < a < n are witnesses
+    #so we test k times, with k = ceil(n/4)
+    k = ceilDiv(n, 4)
+    a = 2
+    while k > 0:
+        if millerWitness(a, n):
+            return False
+        k -= 1
+        a += 1
+    return True
+
+#test if a is a witness for n
+def millerWitness(a, n):
+    (s, d) = findSD(n)
+    x = a**d % n
+    if x == 1:
+        return False
+    while s > 0 :
+        x = x**2 % n
+        if x == 1:
+            return False
+        s -= 1
+    return True
+
+#return s and d as 2^s * d = n-1
+def findSD(n):
+    d = n - 1
+    s = 0
+    while(d % 2 == 0):
+        s += 1
+        d /= 2
+    return (s, d)
+
+#return ceil(a/b)
+def ceilDiv(a, b):
+    return -(-a // b)
