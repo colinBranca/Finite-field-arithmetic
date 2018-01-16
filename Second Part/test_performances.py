8f3231fc75c73bcfd9ac609f3822d68a401413f9
colinBranca <colin.branca@epfl.ch>
2018-01-16 15:59:16 +0100

    Improve script

--- Second Part/test_performances.py
+++ Second Part/test_performances.py
@@ -1,64 +1,61 @@
 import sys
-import timeit
+import time
 import matplotlib.pyplot as plt
 from  rings.polynomials.naive import Polynomials
 from fields.finite.naive import FiniteField
 from support.primes import *
 from random import randrange
 
-#List of 100 first primes
-firstPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
-                61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131,
-                137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197,
-                199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271,
-                277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353,
-                359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433,
-                439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509,
-                521, 523, 541]
+#List of primes (51 to 100 th)
+primes = [3, 7, 13, 31, 61, 127, 251, 509, 1021, 2039,
+        4093, 8191, 16381, 32749, 65521, 131071, 262139, 524287, 999983]
 
 x1 = []
 first_times = []
-GF  = FiniteField( 59 )
+GF  = FiniteField( 449 )
 GFx = Polynomials( GF )
-for deg in range(50, 200, 10):
+for deg in range(5, 201, 5):
+    print(deg)
     sum_times = 0
-    for _ in range(10):
+    for _ in range(5):
         f = GFx.random_monic_poly_degree(deg)
-        start = timeit.timeit()
+        start = time.clock()
         factors = f.factor()
-        stop = timeit.timeit()
+        stop = time.clock()
         sum_times = sum_times + stop - start
     x1.append(deg)
-    first_times.append(sum_times/10)
+    first_times.append(sum_times/5)
+
+
+plt.figure(1)
+plt.plot(x1, first_times)
+plt.xlabel('degree of polynomial')
+plt.ylabel('factorization time (in seconds)')
+plt.savefig("graph1.png")
 
 print("part 1 done ")
 
 x2 = []
 second_times = []
-for p in firstPrimes:
+i = 1
+for p in primes:
+    print(p)
     sum_times = 0
-    for _ in range(10):
+    i = i + 1
+    for _ in range(5):
         GF  = FiniteField( p )
         GFx = Polynomials( GF )
         f = GFx.random_monic_poly_degree(50)
-        start = timeit.timeit()
+        start = time.clock()
         factors = f.factor()
-        stop = timeit.timeit()
+        stop = time.clock()
         sum_times = sum_times + stop - start
-    x2.append(p)
-    second_times.append(sum_times/10)
+    x2.append(i)
+    second_times.append(sum_times/5)
 
-
-plt.figure(1)
-plt.plot(x1, first_times)
-plt.title('Runtimes for polynomials in F17')
-plt.xlabel('degree of polynomial')
-plt.ylabel('factorization time')
-plt.savefig("graph1.png")
 
 plt.figure(2)
 plt.plot(x2, second_times)
-plt.title('Runtimes for polynomials of degree 20')
-plt.xlabel('Finite Field')
-plt.ylabel('factorization time')
+plt.xlabel('Prime length (in bits)')
+plt.ylabel('factorization time (in seconds)')
 plt.savefig("graph2.png")
